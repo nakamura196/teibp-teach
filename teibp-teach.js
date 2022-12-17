f8b3c0f4e25b5fb6f3d765afe54feaea811dc7b4
Satoru <you@example.com>
2022-12-17 21:40:04 +0900

    feat: initial commit

--- teibp-teach.js
+++ teibp-teach.js
@@ -2,7 +2,7 @@ var finalhandler = require('finalhandler')
 var http = require('http')
 var serveStatic = require('serve-static')
  
-var serve = serveStatic('serve', { 'index': ['index.html', 'index.htm'] })
+var serve = serveStatic('docs', { 'index': ['index.html', 'index.htm'] })
  
 var server = http.createServer(function onRequest (req, res) {
   serve(req, res, finalhandler(req, res))
